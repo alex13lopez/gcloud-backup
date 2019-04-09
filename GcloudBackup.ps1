69018d63a95081cb6290c94c0bfdd29f66966823
Alex López <arendevel@gmail.com>
2019-04-09 13:35:01 +0200

    Ver: 6.1a
    - Some bugfixes
    - Logging redone
    - TO DO: Debug File deletion

--- GcloudBackup.ps1
+++ GcloudBackup.ps1
@@ -1,23 +1,26 @@
 # Name: Gcloud Backup
 # Author: Alex López <arendevel@gmail.com> || <alopez@hidalgosgroup.com>
-# Version: 6.0a
+# Version: 6.1a
 
 ########## Var & parms declaration #####################################################
 param(
-	[Parameter(Mandatory = $false)][switch]$clean = $false, 
+    [Parameter(Mandatory = $false)][switch]$all       = $false,
+	[Parameter(Mandatory = $false)][switch]$clean     = $false, 
 	[Parameter(Mandatory = $false)][switch]$removeOld = $false,
-	[Parameter(Mandatory = $false)][switch]$dryRun = $true # For now, we'll always go with dry run mode, until everything works like a charm
+	[Parameter(Mandatory = $false)][switch]$dryRun    = $true
 	)
 	
-$dateLogs      = Get-Date -UFormat "%Y%m%d"
-$logDir        = "C:\Users\Admin\Desktop\GcloudLogs"
-$logFile       = "$logDir\$dateLogs\logFile.txt"
-$errorLog    = "$logDir\$dateLogs\errorLog.txt"
-$cleanLog      = "$logDir\$dateLogs\cleanLogFile.txt"
-$removeErrorLog    = "$logDir\$dateLogs\revomeOldErrorLog.txt"
-$backupPaths   = @("\\172.26.0.97\VeeamBackup\Backup-AX_QV_DC-F","\\172.26.0.97\VeeamBackup\Backup-Resto-F") #TO DO: Add VeeamBackupConfig & Replicas
-$serverPath    = "gs://srvbackuphidreborn/backups"
-$daysToKeepBK  = 8 # 8 days because in case it's Sunday we'll keep the last full backup made on last Saturday
+$dateLogs          = Get-Date -UFormat "%Y%m%d"
+#$logDir            = "C:\Users\Admin\Desktop\GcloudLogs"
+$logDir            = "Y:\test"
+$logFile           = "$logDir\$dateLogs\logFile.txt"
+$errorLog          = "$logDir\$dateLogs\errorLog.txt"
+$cleanLog          = "$logDir\$dateLogs\cleanLogFile.txt"
+$removeLogFile     = "$logDir\$dateLogs\removeLogFile.txt"
+$removeErrorLog    = "$logDir\$dateLogs\removeOldErrorLog.txt"
+$backupPaths       = @("\\172.26.0.97\VeeamBackup\Backup-AX_QV_DC-F","\\172.26.0.97\VeeamBackup\Backup-Resto-F") #TO DO: Add VeeamBackupConfig & Replicas
+$serverPath        = "gs://srvbackuphidreborn/backups"
+$daysToKeepBK      = 8 # 8 days because in case it's Sunday we'll keep the last full backup made on last Saturday
 
 #########################################################################################
 
@@ -25,6 +28,9 @@ function getTime() {
 	return Get-Date -UFormat "%d-%m-%Y @ %H:%M"
 }
 
+function createLogFolder() {
+	mkdir "$logDir\$dateLogs" -ErrorAction Continue 2>&1> $null
+}
 
 function autoClean() {
 
@@ -44,7 +50,7 @@ function autoClean() {
 			$timeNow = getTime
 			echo ("Autocleaning finished at " + $timeNow)
 			
-		} 2> 1 1> $cleanLog
+		} 2>>1 1>> $logFile
 		
 		
 }
@@ -61,31 +67,37 @@ function removeOldBackups() {
 		$timeNow = getTime
 	    echo ("Removing old backup files' job started at " + $timeNow) 1>> $logFile
 		
-		foreach ($file in $files) {
+		&{
 		
-			$fileName = ($file -Split "/")[-1]
-			$fileDate = ((($file -Split "F")[1] -Split "T")[0]) -Replace "[-]"
-			$fileExt = ($fileName -Split ".")[-1]
+			foreach ($file in $files) {
 			
-			if ($fileExt -ne "vbm") { # We skip '.vbm' files since they are always the same and don't have date on it
-				if ($dryRun) {
-					echo "File: $file"
-					echo "FileName: $fileName || fileDate: $fileDate" 
-					echo ""
-				}
+				$fileName = ($file -Split "/")[-1]
+				$fileDate = ((($file -Split "F")[1] -Split "T")[0]) -Replace "[-]"
+				$fileExt = ($fileName -Split ".")[-1]
 				
-				&{
 				
-					if ($fileDate -lt $lastWeek) {
-						echo "The file: '$fileName' is older than $daysToKeepBK days... Wiping out!"
-						if (!$dryRun) {				
-							gsutil -m -q rm -a "$file" # -m makes the operation multithreaded. -q causes gsutil to be quiet, basically: No progress reporting, only errors
+					if ($fileExt -ne "vbm") { # We skip '.vbm' files since they are always the same and don't have date on it
+						if ($dryRun) {
+							echo "File: $file"
+							echo "FileName: $fileName || fileDate: $fileDate" 
+							echo ""
 						}
+						
+						
+						
+							if ($fileDate -lt $lastWeek) {
+								echo "The file: '$fileName' is older than $daysToKeepBK days... Wiping out!"
+								if (!$dryRun) {				
+									gsutil -m -q rm -a "$file" # -m makes the operation multithreaded. -q causes gsutil to be quiet, basically: No progress reporting, only errors
+								}
+							}
+							
+						 
 					}
-					
-				} 2>> $removeErrorLog 1>> $logFile 
+				 
 			}
-		}
+			
+		} 2>> $removeErrorLog 1>> $removeLogFile
 		
 		$timeNow = getTime
 	    echo ("Removing old backup files' job finished at " + $timeNow) 1>> $logFile 
@@ -109,7 +121,7 @@ function doUpload() {
 			$timeNow = getTime
 			echo ("Uploading $dirName to Gcloud... Job started at " + $timeNow)
 			
-			if (!dryRun) {
+			if (!$dryRun) {
 				gsutil -m -q cp -r "$path" "$serverPath/$dirName"
 			}
 			
@@ -120,18 +132,37 @@ function doUpload() {
 		$timeNow = getTime
 		echo ("Uploading Backups to Gcloud... Job Finished at " + $timeNow)
 
-	}  2> $errorLog 1> $logFile
+	}  2>> $errorLog 1>> $logFile
 	
 }
 
-
-if ($clean) {
-	autoClean
-} 
-elseif ($removeOld) {
-	removeOldBackups
-} 
-else {
-	doUpload
+try {
+	if ($clean) {
+		createLogFolder
+		autoClean
+	} 
+	elseif ($removeOld) {
+		createLogFolder
+		removeOldBackups
+	}
+	elseif ($All) {
+		createLogFolder
+		autoClean
+		doUpload
+		removeOldBackups
+	}
+	else {
+		createLogFolder
+		doUpload
+	}
 }
+catch [System.IO.DirectoryNotFoundException] {
+	Write-Host 'Please, check that file paths are well configured' -fore red -back black
+}
+catch {
+	# We catch all exceptions and show the fullname of the exception so we can handle it better
+	Write-Host 'Unknown error. Caught exception:' $_.Exception.GetType().FullName -fore red -back black
+}
+
+
 
