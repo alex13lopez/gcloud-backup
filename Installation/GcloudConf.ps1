fad1461365511efee4f7b987c58052d525ed1589
Alex López <arendevel@gmail.com>
2019-08-05 16:33:17 +0200

    Cleaning up

--- Installation/GcloudConf.ps1
+++ Installation/GcloudConf.ps1
@@ -8,8 +8,8 @@ $cleanLog          = '$logDir\$dateLogs\cleanLogFile.txt'
 $removeLogFile     = '$logDir\$dateLogs\removeLogFile.txt'
 $removeErrorLog    = '$logDir\$dateLogs\removeOldErrorLog.txt'
 $credErrorLog	   = '$logDir\$dateLogs\credErrorLog.txt'
-$backupPaths       = @('Z:\Backups\SRVAXAPTA', 'Z:\Backups\SERVERTS', 'Z:\Backups\SRVDATOS', 'Z:\Backups\SRVDC', 'Z:\Backups\QLIKSERVER', 'Z:\Backups\SRVAPPS', 'Z:\Backups\vCenter', 'Z:\Backups\SRVVEEAM')
-$serverPath        = 'gs://srvbackuphidreborn/backups'
+$backupPaths       = @('', '') # Comma-separated paths
+$serverPath        = ''
 $daysToKeepBK      = 8 # 8 days because in case it's Sunday we'll keep the last full backup made on last Saturday
 
 # Mailing Options
