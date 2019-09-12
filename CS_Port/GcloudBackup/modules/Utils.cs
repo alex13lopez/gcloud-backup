cd34085525cdba8164a6fff118a0af1403eee96f
Alex López <alexlopezmelo@gmail.com>
2019-09-12 14:12:52 +0200

    Ver: 0.0.1.3a
    - Credentials module finished, need testing

--- CS_Port/GcloudBackup/modules/Utils.cs
+++ CS_Port/GcloudBackup/modules/Utils.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Text;
 using System.Security.Cryptography;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Utilities
 {
@@ -19,16 +21,23 @@ namespace Utilities
     class Credentials
     {
         private string credDir;
-        private string usrFile;
-        private string pwFile;
+        private string credFile;
+        private string user;
+        private string password;
         private static readonly byte[] entropy = { 9, 2, 4, 1, 3, 5, 0 };
 
+        public string User { get => user; set => user = value; }
 
-        public Credentials(string cDir)
+        public string Password {
+            // We make an accessor to store the password in case we want to set credentials in interactive mode
+            get => "Password is encrypted so it cannot be shown.";
+            set => password = value;
+        }
+
+        public Credentials(string cDir, string fileName)
         {
             credDir = cDir;
-            usrFile = Path.Combine(cDir, "Username");
-            usrFile = Path.Combine(cDir, "Password");
+            credFile = Path.Combine(cDir, fileName);
         }
 
         private string encrypt(string txt)
@@ -60,9 +69,32 @@ namespace Utilities
             return password;
         }
 
-        private void saveCredentials(string usr, string passwd)
+        private void saveCredentials()
         {
+            File credentialsFile = new File(this.credFile);
+            Stream fileStream = credentialsFile.Open(FileMode.Create);
+            BinaryFormatter binFormatter = new BinaryFormatter();
 
+            binFormatter.Serialize(fileStream, this);
+            fileStream.Close();
+
+        }
+
+        private string loadCredentials()
+        {
+            if (chkCredentials())
+            {
+                
+                File credentialsFile = new File(this.credFile);
+                Stream fileStream = credentialsFile.Open(FileMode.Open);
+                BinaryFormatter binFormatter = new BinaryFormatter();
+
+                this = (Credentials)binFormatter.Deserialize(fileStream); // I have to try if this works... Idk if it will let me overwrite the current instance of Credentials()
+                fileStream.Close();
+
+                return "Credentials were successfully loaded";
+            }
+            return "Credentials were not found.";
         }
 
         public void genCredentials()
@@ -78,13 +110,14 @@ namespace Utilities
             Console.WriteLine("\t\tPassword: ");
             passwd = encrypt(getPassword()); // We save the value already encrypted, so nobody can steal it from memory
 
-            saveCredentials(user, passwd);
+            this.User = user;
+            this.Password = passwd;
+
+            saveCredentials();
 
         }
 
-        
-
-        public Boolean chkCredentials() => File.Exists(usrFile) && File.Exists(pwFile);
+        public Boolean chkCredentials() => File.Exists(this.credFile);
 
 
     }
